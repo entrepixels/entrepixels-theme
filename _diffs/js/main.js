f549946eb9162474c28af0b6f79657dba74a66c0
Marcos <marcos.castro@liferay.com>
2015-09-25 14:46:16 +0200

    Remove unneeded spaces from main.js

--- _diffs/js/main.js
+++ _diffs/js/main.js
@@ -1,31 +1,31 @@
 AUI().ready(
 	function(A) {
-		
-		/* Show and hide child-menu */		
+
+		/* Show and hide child-menu */
 		if(A.one ('span.child-button')){
-			A.all('span.child-button').each(function (node) {			
+			A.all('span.child-button').each(function (node) {
 				node.on('click', function(node){
 					if(this.hasClass('up')){
 						A.all('.child-menu').setStyle("display", "none");
 						A.all('#wrapper #navigation li span.child-button').removeClass('down');
 						A.all('#wrapper #navigation li span.child-button').addClass('up');
-						
+
 						this.removeClass('up');
 						this.addClass('down');
 						this.siblings('.child-menu').setStyle("display", "block");
-						
+
 					}
 					else{
 						this.removeClass('down');
 						this.addClass('up');
 						this.siblings('.child-menu').setStyle("display", "none");
-						
+
 					}
-					
+
 				})
 			});
 		}
-		
+
 		/* Show and hide navigation */
 		if(A.one ('.navbar-btn-nav')){
 			A.one('.navbar-btn-nav').on('click',function(){
@@ -34,12 +34,9 @@ AUI().ready(
 				}else{
 					A.one('#navigation').setStyle("display", "none")
 				}
-				
+
 			});
 		}
-		
-	}
-);
 
 	}
 );
